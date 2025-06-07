770815c3b8b440f3bfa1094fb7c8a948ad2eadff
phquartin <phquartinmatos@sempreceub.com>
2025-06-07 17:39:29 -0300

    init: Configs iniciais

---
+++ src/main/java/com/libseek/LibSeekApplication.java
@@ -0,0 +1,13 @@
+package com.libseek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibSeekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibSeekApplication.class, args);
+    }
+
+}
